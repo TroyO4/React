dc47e24509037dff0a42e2b98e59a54726c08e3f
troy <oconnort@go.stockton.edu>
2025-06-18 19:07:25 -0400

    Home page

--- portfolioSite/src/pages/Home.jsx
+++ portfolioSite/src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import './Home.css';
+import { Link } from 'react-router-dom'; // ✅ Add this
 
 const Home = () => {
   return (
     <div className="homeContainer">
-      <h1>Hello World</h1>
+      <div className="title">
+        <h1>Web Developer</h1>
+        <h3>
+          Take a look at my personal site and check out some of my other
+          projects{' '}
+          <Link to="/projects" className="link">
+            here
+          </Link>
+        </h3>
+      </div>
     </div>
   );
 };
