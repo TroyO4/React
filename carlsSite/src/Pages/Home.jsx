aec82f341631b0b6f202adbad36cee0db6a615d4
troy <oconnort@go.stockton.edu>
2025-06-19 22:27:12 -0400

    Home page

--- carlsSite/src/Pages/Home.jsx
+++ carlsSite/src/Pages/Home.jsx
@@ -1,11 +1,26 @@
 import './Home.css';
-
 import React from 'react';
 
 const Home = () => {
   return (
-    <div className="HomeCont">
-      <h1>Carl Deworsop</h1>
+    <div className="homeCont">
+      <div className="titleCont">
+        <video autoPlay loop muted playsInline className="video">
+          <source src="/images/constrVid.mp4" type="video/mp4" />
+        </video>
+        <div className="titleText">
+          <h1 className="roca">Roca</h1>
+          <h1 className="constLLC">Construction LLC</h1>
+          <h2>Robert DelGado & Carl Deworsop</h2>
+        </div>
+      </div>
+
+      <div className="bodyCont">
+        <h1>Recent Projects</h1>
+        <p>
+          sdfsdddsfafrfnjfkhiufbkbciubciubciufbiubfciudbciubiucakgfbiuldksgaufgbkerugfsdbk,gfbek,sfbiskfbliusfkbiksbflibsdfsdddsfafrfnjfkhiufbkbciubciubciufbiubfciudbciubiucakgfbiuldksgaufgbkerugfsdbk,gfbek,sfbiskfbliusfkbiksbflibsdfsdddsfafrfnjfkhiufbkbciubciubciufbiubfciudbciubiucakgfbiuldksgaufgbkerugfsdbk,gfbek,sfbiskfbliusfkbiksbflibsdfsdddsfafrfnjfkhiufbkbciubciubciufbiubfciudbciubiucakgfbiuldksgaufgbkerugfsdbk,gfbek,sfbiskfbliusfkbiksbflibsdfsdddsfafrfnjfkhiufbkbciubciubciufbiubfciudbciubiucakgfbiuldksgaufgbkerugfsdbk,gfbek,sfbiskfbliusfkbiksbflibsdfsdddsfafrfnjfkhiufbkbciubciubciufbiubfciudbciubiucakgfbiuldksgaufgbkerugfsdbk,gfbek,sfbiskfbliusfkbiksbflibsdfsdddsfafrfnjfkhiufbkbciubciubciufbiubfciudbciubiucakgfbiuldksgaufgbkerugfsdbk,gfbek,sfbiskfbliusfkbiksbflibsdfsdddsfafrfnjfkhiufbkbciubciubciufbiubfciudbciubiucakgfbiuldksgaufgbkerugfsdbk,gfbek,sfbiskfbliusfkbiksbflibsdfsdddsfafrfnjfkhiufbkbciubciubciufbiubfciudbciubiucakgfbiuldksgaufgbkerugfsdbk,gfbek,sfbiskfbliusfkbiksbflib
+        </p>
+      </div>
     </div>
   );
 };
